ee9fce141b8a15aaf0736f1ea27be4417d52ca59
vincenteliang <vincenteljh@gmail.com>
2020-07-07 17:39:35 +0800

    3-2 axios 封装 & 数据获取

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
   </div>
 </template>
 
 <script>
-import VHeader from "./components/v-header/v-header";
+  import VHeader from './components/v-header/v-header'
+  import {getSeller} from 'api'
 
-export default {
-  name: 'App',
-  components: {
-    VHeader
+  export default {
+    name: 'App',
+    data() {
+      return {
+        seller: {}
+      }
+    },
+    created() {
+      this._getSeller()
+    },
+    methods: {
+      _getSeller() {
+        getSeller().then((seller) => {
+          // 由于用 axios 封装了一遍，这里一定可以拿到数据
+          this.seller = seller
+        })
+      }
+    },
+    components: {
+      VHeader
+    }
   }
-}
 </script>
 
 <style lang="stylus">
