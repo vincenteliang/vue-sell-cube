3e4fee3bbd683cd161b1b8725f9d408992d6062c
vincenteliang <vincenteljh@gmail.com>
2020-07-07 17:49:42 +0800

    3-3 header-detail & star 组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
+    <header-detail :seller="seller"></header-detail>
   </div>
 </template>
 
 <script>
   import VHeader from './components/v-header/v-header'
+  import HeaderDetail from './components/header-detail/header-detail'
   import {getSeller} from 'api'
 
   export default {
@@ -27,6 +29,7 @@
       }
     },
     components: {
+      HeaderDetail,
       VHeader
     }
   }
